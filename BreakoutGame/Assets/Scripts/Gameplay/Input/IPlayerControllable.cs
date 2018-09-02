5b0bc607767d3c341694430d6ae4e0c26acd5ffb
PJ <pj.haye@gmail.com>
2018-09-01 21:46:08 -0700

    Add player-controllable interface and add it to Paddle

---
+++ BreakoutGame/Assets/Scripts/Gameplay/Input/IPlayerControllable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BreakoutGame
+{
+    public interface IPlayerControllable
+    {
+        void OnAxisInput(Vector2 axis);        
+    }
+}
--- BreakoutGame/Assets/Scripts/Gameplay/Paddle/Paddle.cs
+++ BreakoutGame/Assets/Scripts/Gameplay/Paddle/Paddle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace BreakoutGame
 {
-    public class Paddle : MonoBehaviour
+    public class Paddle : MonoBehaviour, IPlayerControllable
     {
 
         // Use this for initialization
@@ -22,6 +22,11 @@ namespace BreakoutGame
         public void SetWidth(float value)
         {
             transform.localScale = new Vector3(value, 1.0f, 1.0f);
+        }
+
+        public void OnAxisInput(Vector2 axis)
+        {
+            
         }
     }
 }
