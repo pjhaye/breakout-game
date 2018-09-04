34adb324019b02f813ed853da6192bfc4cd6f987
PJ <pj.haye@gmail.com>
2018-09-03 23:54:11 -0700

    Create Skybox

---
+++ BreakoutGame/Assets/Scripts/Utilities/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BreakoutGame
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _rotationPerSecond;
+
+        private void FixedUpdate()
+        {
+            transform.Rotate(_rotationPerSecond * Time.deltaTime, Space.Self);
+        }
+    }
+}
